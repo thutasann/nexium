b34e9129aebe2a349e52870729330536b76550eb
thutasann <thutasann2002@gmail.com>
2024-10-31 00:44:51 +0800

    add(gha): build examples

--- __test__/uuid.test.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-const { NUUId } = require('../lib')
-
-describe('UUID Functions', () => {
-  test('should generate a valid UUID', () => {
-    const uuid = NUUId.generate()
-    expect(uuid).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
-  })
-})
