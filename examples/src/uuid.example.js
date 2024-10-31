c31c9096b1a5612dd1e948a399bea4b27f6e1075
thutasann <thutasann2002@gmail.com>
2024-10-31 15:06:00 +0800

    release: v1.0.18

--- examples/src/uuid.example.js
+++ examples/src/uuid.example.js
@@ -7,6 +7,18 @@ function uuid_examples() {
 
   const uuid = NUUId.generate()
   console.log('uuid', uuid)
+
+  const uuid_string = 'e82faf28-b22b-44fe-a55b-0e9ead917d79'
+  const parsed = NUUId.parse(uuid_string)
+  console.log('parsed', parsed)
+
+  const validUUID = 'e82faf28-b22b-44fe-a55b-0e9ead917d79'
+  const isValid = NUUId.isValid(validUUID)
+  console.log('isValid', isValid)
+
+  const bytes = [0xe8, 0x2f, 0xaf, 0x28, 0xb2, 0x2b, 0x44, 0xfe, 0xa5, 0x5b, 0x0e, 0x9e, 0xad, 0x91, 0x7d, 0x79]
+  const stringified_uuid = NUUId.bytesToUUID(bytes)
+  console.log('stringified_uuid', stringified_uuid)
 }
 
 module.exports = { uuid_examples }
