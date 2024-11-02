31c76518bf7859392ce57c9e069057151d4011d1
thutasann <thutasann2002@gmail.com>
2024-11-03 03:35:45 +0800

    release: v.1.0.23

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,7 @@
 // @ts-check
 const { arrayExamples } = require('./src/array.example')
 const { mathsExamples } = require('./src/maths.example')
+const { numberExamples } = require('./src/number.example')
 const { stringExamples } = require('./src/string.example')
 const { uuidExamples } = require('./src/uuid.example')
 
@@ -8,3 +9,4 @@ mathsExamples()
 stringExamples()
 uuidExamples()
 arrayExamples()
+numberExamples()
