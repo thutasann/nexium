4c9a3598d24939214d14b9cd1eed55d559045b88
thutasann <thutasann2002@gmail.com>
2024-11-02 17:39:36 +0800

    fix(gha): generate uuid vanilla

--- __test__/benchmark/string.js
+++ __test__/benchmark/string.js
@@ -4,7 +4,11 @@ const { benchmark } = require('./benchmark_fn')
 
 /** @private Vanilla JavaScript UUID generator using `crypto.randomUUID` */
 function generateUUIDVanilla() {
-  return crypto.randomUUID()
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }
 
 /** String Benchmark Test */
