514edb15715c5938b9eda0795d2ef3ecb6d95341
thutasann <thutasann2002@gmail.com>
2024-11-03 22:04:04 +0800

    add: number examples

--- examples/src/number.example.js
+++ examples/src/number.example.js
@@ -10,6 +10,24 @@ function numberExamples() {
 
   const inRageResult = NNumber.inRange(5, 1, 10)
   console.log('inRageResult', inRageResult)
+
+  const fibonacci = NNumber.fibonacci(50)
+  console.log('fibonacci', fibonacci)
+
+  const sum_result = NNumber.sum([-1, -2, 3, 4, 5])
+  console.log('sum_result', sum_result)
+
+  const ordinal_result = NNumber.toOrdinal(111)
+  console.log('ordinal_result', ordinal_result)
+
+  const random = NNumber.generateRandom(1, 100)
+  console.log('random', random)
+
+  const roundTo = NNumber.roundTo(3.14159, 2)
+  console.log('roundTo', roundTo)
+
+  const inRange = NNumber.inRange(10, 1, 10)
+  console.log('inRange', inRange)
 }
 
 module.exports = { numberExamples }
